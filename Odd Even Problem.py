f11ab4720677767e82d40c9961a3daacb1a6194e
Harshit-Dhundale <120770433+Harshit-Dhundale@users.noreply.github.com>
2024-06-01 12:00:47 +0530

    Create Odd Even Problem.py

---
+++ Odd Even Problem.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def oddEven(self, s : str) -> str:
+        # code here
+        count = Counter(s)
+        ans = 0
+        
+        for key in count:
+            if count[key] & 1 == ord(key) & 1: ans += 1
+        
+        return "ODD" if ans & 1 else "EVEN"
