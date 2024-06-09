66551762728a4d0c92dcd6497aa5ac50a89b7a58
Harshit Dhundale <120770433+Harshit-Dhundale@users.noreply.github.com>
2024-06-09 17:01:37 +0530

    Added Solution - GfG to GitHub

---
+++ School/Distance between 2 points/distance-between-2-points.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+class Solution:
+    def distance(self, x1, y1, x2, y2):
+        return round(((x1-x2)**2 + (y1-y2)**2)**0.5)
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		x1, y1, x2, y2 = input().split()
+		x1 = int(x1);
+		y1 = int(y1);
+		x2 = int(x2);
+		y2 = int(y2);
+		ob = Solution();
+		ans = ob.distance(x1, y1, x2, y2)
+		print(ans)
+# } Driver Code Ends
