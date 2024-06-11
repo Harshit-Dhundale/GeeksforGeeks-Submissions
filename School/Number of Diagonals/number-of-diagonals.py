195fd4ef45ef10d18ce8b2417dc41da41bd62d19
Harshit Dhundale <120770433+Harshit-Dhundale@users.noreply.github.com>
2024-06-11 13:21:02 +0530

    Added Solution - GfG to GitHub

---
+++ School/Number of Diagonals/number-of-diagonals.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+
+class Solution:
+    def diagonals(self, n):
+        return int(n*(n-3)//2)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        ob = Solution()
+        print(ob.diagonals(n))
+# } Driver Code Ends
