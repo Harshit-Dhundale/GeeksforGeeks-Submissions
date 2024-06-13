299a1751155c72296c4f4c869271fac07514d7af
Harshit Dhundale <120770433+Harshit-Dhundale@users.noreply.github.com>
2024-06-13 12:35:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Padovan Sequence/padovan-sequence.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+class Solution:
+    def padovanSequence(self, n):
+        m=1000000007
+        if n<=2:
+            return 1
+        else:
+            a,b,c=1,1,1
+            d=0
+            for i in range(3,n+1):
+                d=(a%m+b%m)%m
+                a,b,c=b,c,d
+        return d
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        ob = Solution()
+        print(ob.padovanSequence(n))
+
+# } Driver Code Ends
