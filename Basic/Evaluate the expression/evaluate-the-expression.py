a2766c99432a5f0ea0aa4f13ceab403c87df56ac
Harshit Dhundale <120770433+Harshit-Dhundale@users.noreply.github.com>
2024-06-14 11:22:43 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Evaluate the expression/evaluate-the-expression.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+class Solution:
+
+ def evaluate_exp(self, n):
+    
+     if n&1:
+         return -1
+     return 1
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		ob = Solution()
+		ans = ob.evaluate_exp(n)
+		print(ans)
+# } Driver Code Ends
