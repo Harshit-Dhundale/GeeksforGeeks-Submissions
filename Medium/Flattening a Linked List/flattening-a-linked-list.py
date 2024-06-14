15ed4b99691135517f3efbff9dc6e8c41f142fac
Harshit Dhundale <120770433+Harshit-Dhundale@users.noreply.github.com>
2024-06-14 11:25:27 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Flattening a Linked List/flattening-a-linked-list.py
@@ -0,0 +1,93 @@
+#User function Template for python3
+
+
+def mergeTwoSortedList(l1, l2):
+    if not l1:
+        return l2
+    if not l2:
+        return l1
+    result = Node(None)
+    if l1.data < l2.data:
+        result = l1
+        result.bottom = mergeTwoSortedList(result.bottom, l2)
+    else:
+        result = l2
+        result.bottom = mergeTwoSortedList(l1, result.bottom)
+    return result
+    
+def flatten(root):
+    if not root:
+        return None
+    root2 = flatten(root.next)
+    return mergeTwoSortedList(root, root2)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+class Node:
+    def __init__(self, d):
+        self.data=d
+        self.next=None
+        self.bottom=None
+        
+        
+
+def printList(node):
+    while(node is not None):
+        print(node.data,end=" ")
+        node=node.bottom
+        
+    print()
+
+
+if __name__=="__main__":
+    t=int(input())
+    while(t>0):
+        head=None
+        N=int(input())
+        arr=[]
+        
+        arr=[int(x) for x in input().strip().split()]
+        temp=None
+        tempB=None
+        pre=None
+        preB=None
+        
+        flag=1
+        flag1=1
+        listo=[int(x) for x in input().strip().split()]
+        it=0
+        for i in range(N):
+            m=arr[i]
+            m-=1
+            a1=listo[it]
+            it+=1
+            temp=Node(a1)
+            if flag == 1:
+                head=temp
+                pre=temp
+                flag=0
+                flag1=1
+            else:
+                pre.next=temp
+                pre=temp
+                flag1=1
+                
+            for j in range(m):
+                a=listo[it]
+                it+=1
+                tempB=Node(a)
+                if flag1 == 1:
+                    temp.bottom=tempB
+                    preB=tempB
+                    flag1=0
+                else:
+                    preB.bottom=tempB
+                    preB=tempB
+        root=flatten(head)
+        printList(root)
+        
+        t-=1
+            
+# } Driver Code Ends
